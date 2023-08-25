7563add5869dcbb33e3bac1450cf6edd225cc026
cespedesBrian <cepedesaguilerabrian@gmail.com>
2023-08-25 15:58:30 -0300

    Se hizo la configuración basica del app.js

---
+++ app.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+
+// Middleware
+
+app.use(cors());
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
+app.use(morgan('dev'));
+app.use(express.json());
+
+// Routes
+
+// Starting the server
+const PORT = 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
