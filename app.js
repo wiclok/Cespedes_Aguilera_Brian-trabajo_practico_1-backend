30a284c397994da626172e85cecde44fb55eb929
cespedesBrian <cepedesaguilerabrian@gmail.com>
2023-08-25 16:04:22 -0300

    Se agregaron las variables de entorno

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.json());
 // Routes
 
 // Starting the server
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
